97af324995f37086a9781eed66cf8c635daf39ee
Mattijs Spierings <mattijsspierings@gmail.com>
2018-01-03 17:14:16 +1100

    update tsconfig and added zone

--- client/main.ts
+++ client/main.ts
@@ -1,6 +1,6 @@
 import { enableProdMode } from '@angular/core';
-// import 'angular2-meteor-polyfills';
 import 'core-js/es6/set';
+import 'zone.js/dist/zone';
 // import "@angular/material/prebuilt-themes/indigo-pink.css";
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
